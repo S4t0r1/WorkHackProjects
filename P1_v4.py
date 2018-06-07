7ad5d219a3aa6a4ad42f3bd8bc7c2d8cea238c11
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-06-08 00:53:38 +0200

    Update P1_v4.py

--- P1_v4.py
+++ P1_v4.py
@@ -1,29 +1,32 @@
+
 import sys
 
 
-
 class BadInputError(Exception): pass
+
 class Tools():
     def inpt_node_args(self):
-        return {k: None for k in "inType;table;inData;lastIn"}
+        return {k: None for k in "inType;table;inData".split(';')}
     
     def getCommandSet(self, s, char, p=False):
         return set(','.join(c1+c2 if p else ','.join((c1, c1+c2)) 
                    for c1, c2 in zip(s, char*len(s))).split(','))
     
-    def anySetter(self, *conds, seq=None):
-        if len(conds) == 1:
-            return any(len(txt) == conds[0] if type(conds[0]) == int
-                  else len(txt) == txt.count(conds[0]) for txt in seq)
-        return any(len(txt) == conds[0] 
-                or len(txt) in {txt.count(ch) for ch in conds[1]} for txt in seq)
+    def anySetter(self, cond1=None, cond2=None, seq=None):
+        if not cond2:
+            return any(len(txt) == cond1 if type(cond1) == int
+                  else len(txt) == txt.count(cond1) if len(cond1) == 1 
+                  else len(txt) in {txt.count(ch) for ch in cond1 if len(txt) >= 1} for txt in seq)
+        else:
+            return any(len(txt) == cond1
+                or len(txt) in {txt.count(ch) for ch in cond2 if len(txt) >= 1} for txt in seq)
 
-    def inputChecker(self, msg, seq=None, anyfiltr=None):
+    def inputChecker(self, msg, seq=None, filtr=None):
         inpt = input(msg)
+        forbidden_or_allowed = {c for c in "bde"}
         if not seq:
-            return '' if anyfiltr else inpt
-        if len(inpt) in {inpt.count(c) for c in "bde"}:
-            seq = seq | {c*len(inpt) for c in "bde"}
+            return inpt
+        seq = seq | forbidden_or_allowed
         if inpt not in list(seq):
             print("\nERROR: Bad Input!")
             return self.inputChecker(msg, seq)
@@ -33,7 +36,7 @@ class Tools():
 class InputNode(Tools):
     def __init__(self):
         inpt_specs = Tools.inpt_node_args(self)
-        self.__dict__.update({k: None for k in inpt_specs})
+        self.__dict__.update(inpt_specs)
 
     def inType(self):
         return self.inType
@@ -44,25 +47,18 @@ class InputNode(Tools):
     def inData(self):
         return self.inData
 
-    def lastIn(self):
-        return self.lastIn
-
 
 class InputOps(InputNode, Tools):   
     def __init__(self, tools, data):
         tools.__init__()
         InputNode.__init__(self)
         self.tools = tools
-        self.data = data
-    
+
     def getInType(self):
         tools = self.tools
-        if self.inType not in tools.getCommandSet("m", 'a', p=True):
+        if self.inType not in tools.getCommandSet("mf", 'a', p=True):
             msg = '\n'+"INPUT"+'\n'+"manually(m/all=ma)"+'\n'+"    file(f/all=fa): "
             self.inType = tools.inputChecker(msg=msg, seq=tools.getCommandSet("mf", 'a'))
-        else:
-            self.inType = 'ma'
-        self.lastIn = self.inType
         return self.inType
     
     def getTable(self):
@@ -70,48 +66,32 @@ class InputOps(InputNode, Tools):
         if self.table not in tools.getCommandSet("tn", 'a', p=True):
             msg = "Table?(t, ta): "
             self.table = tools.inputChecker(msg=msg, seq=tools.getCommandSet("tn", 'a'))
-        else:
-            self.table = 'ta'
-        self.lastIn = self.table
         return self.table
     
-    def getInData(self):
+    def getInData(self, cnum):
         tools = self.tools
-        msg = "Paste Dataset: " if self.inType in tools.getCommandSet("m", 'a') else "Filename: "
-        self.inData = tools.inputChecker(msg=msg)
-        self.lastIn = self.inData
+        msg = ("Paste Dataset: " if self.inType in tools.getCommandSet("m", 'a') 
+               else "Filename: ")
+        self.inData = tools.inputChecker(msg=msg, filtr=True)
+        self.inData = self.file_mngr(count=cnum)
         return self.inData
     
-    def file_mngr(self, datalist=None, filename=None):
-        tools, count = self.tools, len(datalist)
-        if any(self.table == c for c in tools.getCommandSet('t', 'a')):
+    def file_mngr(self, count=None, filename=None):
+        tools = self.tools
+        if any(self.table == c for c in tools.getCommandSet('tf', 'a')):
             try:
                 filename = self.inData if not filename else filename
                 with open(filename, 'r', encoding='utf8') as fi:
                     self.inData = (fi.readlines() if self.table in tools.getCommandSet('t', 'a') 
                                    else fi.read())
-            except FileNotFoundError:
+            except (FileNotFoundError, OSError):
                 filename = "g_file"+str(count+1)+".txt"
                 with open(filename, 'w', encoding='utf8') as fi:
                     fi = fi.write(self.inData)
             else:   
                 return self.inData
         return self.inData 
-    
-    def addNew(self, datalst=None):
-        tools = self.tools
-        self.inData = self.getInData()
-        if not tools.anySetter(0, "de", seq=[self.inData]):
-            self.inData = self.file_mngr(datalist=datalst)
-            self.all_data = Data(tools).addData(self.inData)
-            self.lastIn = self.inData
-            return self.inData
-        else:
-            print(self.all_data)
-            return 1
-    
-    def del_all(self):
-        return True if self.lastIn == 'da' else False
+
 
 class Data(InputOps):   
     def __init__(self, tools, name=None):
@@ -126,25 +106,52 @@ class Data(InputOps):
         print("{}\n{}".format(self.all_data, self.name))
         return self.all_data
     
-    def addData(self, add):
-        self.all_data.append(add)
-        self.count = len(self.all_data)
-        print("\n{}\ndatapackage{}= {}".format(self.all_data, 
-                              's' if self.count>1 else '', self.count))
+    def addData(self, newdata):
+        if type(newdata) == str:
+            self.all_data.append(newdata)
+            self.count = len(self.all_data)
+            print("\n{}\ndatapackage{}= {}".format(self.all_data, 
+                                    's' if self.count>1 else '', self.count))
+            return self.all_data
     
+    def dellData(self, count):
+        for i in range(count):
+            del self.all_data[-1]
+        print("..deleted {} datablocks".format(count))
+        return self.all_data
+
     def del_allData(self):
-        if self.inData == 'da':
-            print("DELETED all ({}) inputs".format(len(self.all_data)))
-            self.all_data.clear()
+        print("DELETED all ({}) inputs".format(len(self.all_data)))
+        self.all_data.clear()
         return self.all_data
 
 
-def gatherData():
-    d = Data(Tools())
-    while True:
-        x = InputOps(Tools(), InputNode())
-        x.getInType()
-        x.getTable()
-        newData = x.getInData()
-        d.addData(newData)
+def gatherData(datarealm=None):         
+    tools = Tools()
+    _D = Data(tools) if not datarealm else datarealm
+    data = _D.allData()
+    inpt = 'n/a'
+    x = InputOps(tools, InputNode())
+    while not (tools.anySetter("bde", seq=[inpt])) and inpt != "da":
+        print(data)
+        inpt = x.getInType()
+        inpt = x.getTable()
+        inpt = x.getInData(len(data))
+        if tools.anySetter("bde", seq=[inpt]):
+            print(inpt)
+        data = _D.addData(inpt)
+
+    print("woo")    
+    count = len(inpt)
+    if inpt == "da":
+        data = _D.del_allData()
+        return gatherData(datarealm=_D)
+    elif tools.anySetter("d", seq=[inpt]):
+        data = _D.dellData(count)
+        return gatherData(datarealm=_D)
+    elif inpt == 'b':
+        return gatherData(datarealm=_D)
+    else:
+        return sys.exit()
+        
 gatherData()
