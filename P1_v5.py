38d27def964536ff663fdab39fed394e0e3ae756
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-06-08 19:05:09 +0200

    Update P1_v5.py

--- P1_v5.py
+++ P1_v5.py
@@ -4,10 +4,7 @@ import sys
 
 class BadInputError(Exception): pass
 
-class Tools():
-    def inpt_node_args(self):
-        return {k: None for k in "inType;table;inData".split(';')}
-    
+class Tools():  
     def getCommandSet(self, s, char, p=False):
         return set(','.join(c1+c2 if p else ','.join((c1, c1+c2)) 
                    for c1, c2 in zip(s, char*len(s))).split(','))
@@ -24,14 +21,14 @@ class Tools():
     def inputChecker(self, msg, seq=None, filtr=None):
         inpt = input(msg)
         forbidden_or_allowed = {c for c in "bde"} | {'da'}
-        if not seq:
-            if self.anySetter("bde", seq=[inpt]) or inpt == 'da':
-                return inpt, len(inpt)
-        else:
+        if seq and not filtr:
             seq = seq | forbidden_or_allowed
             if inpt not in list(seq):
                 print("\nERROR: Bad Input!")
                 return self.inputChecker(msg, seq)
+        else:
+            if self.anySetter("bde", seq=[inpt]) or inpt == 'da':
+                return inpt, len(inpt)
         return inpt
 
 
@@ -45,19 +42,19 @@ class InputNode(Tools):
     def getInType(self):
         if self.inType not in self.getCommandSet("mf", 'a', p=True):
             msg = '\n'+"INPUT"+'\n'+"manually(m/all=ma)"+'\n'+"    file(f/all=fa): "
-            self.inType = self.inputChecker(msg=msg, seq=self.getCommandSet("mf", 'a'))
+            self.inType = self.inputChecker(msg=msg, seq=self.getCommandSet("mf", 'a'), filtr=True)
         return self.inType
     
     def getTable(self):
         if self.table not in self.getCommandSet("tn", 'a', p=True):
             msg = "Table?(t, ta): "
-            self.table = self.inputChecker(msg=msg, seq=self.getCommandSet("tn", 'a'))
+            self.table = self.inputChecker(msg=msg, seq=self.getCommandSet("tn", 'a'), filtr=True)
         return self.table
     
     def getInData(self, cnum):
         msg = ("Paste Dataset: " if self.inType in self.getCommandSet("m", 'a') 
                else "Filename: ")
-        self.inData = self.inputChecker(msg=msg)
+        self.inData = self.inputChecker(msg=msg, filtr=True)
         return self.file_mngr(count=cnum) if len(list(self.inData)) == 1 else self.inData
     
     def file_mngr(self, count=None, filename=None):
@@ -82,18 +79,16 @@ class Data(InputNode):
         self.name = name
         self.commands = Tools.getCommandSet(self, "mftda", 'a')
         self.all_data = []
-        self.actionsD = {}
         self.count = 0
     
     def print_allData(self):
         return "\n{}\n{}".format(self.name, self.all_data)         
     
-    def back_startmenu(self, inpt):
-        self.actionsD[inpt] = InputNode.__init__(self), self.getInputs()
-        return self.actionsD[inpt]
+    def back_startmenu(self):
+        InputNode.__init__(self)
+        return self.getInputs()
     
-    def exit_adding(self, inpt):
-        self.actionsD[inpt] = "ext"
+    def exit_adding(self):
         return "ext"
 
     def addData(self, newdata):
@@ -101,31 +96,30 @@ class Data(InputNode):
         print("\n{}\n{}\ndatapackage{} = {}".format(self.name, self.all_data, 
                   's' if len(self.all_data) > 1 else '', len(self.all_data)))
     
-    def dellData(self, inpt, count):
-        self.actionsD[inpt] = self.all_data[:-count]
-        self.all_data = self.actionsD[inpt]
+    def dellData(self, count):
+        self.all_data = self.all_data[:-count]
         print("..deleted {} datablocks\n{}".format(count, self.print_allData()))
 
-    def del_allData(self, inpt, count):
-        self.actionsD[inpt] = self.all_data.clear()
+    def del_allData(self, count):
+        self.all_data.clear()
         print("..deleted all inputs({})\n{}".format(count, self.print_allData()))
     
     def getOperation(self, inpt, count):
-        return (self.back_startmenu(inpt) if 'b' in inpt
-           else self.del_allData(inpt, count) if 'da' == inpt
-           else self.dellData(inpt, count) if 'd' in inpt
-           else self.exit_adding(inpt))
+        new_op = (self.back_startmenu() if 'b' in inpt
+              else self.del_allData(count) if 'da' == inpt
+              else self.dellData(count) if 'd' in inpt
+              else self.exit_adding())
+        return new_op
 
     def getInputs(self):
         exit_datarealm = None
         while not exit_datarealm:
-            print(self.actionsD)
             for inpt in (self.getInType(), self.getTable(), self.getInData(self.count)):
                 if type(inpt) == tuple:
+                    print("foo")
                     inpt, count = inpt
                     operation = self.getOperation(inpt, count)
-                    exit_datarealm = operation if operation == 'ext' else exit_datarealm
-                    self.actionsD.get(inpt, operation)
+                    exit_datarealm = operation if operation == 'ext' else None
                     break
             else:
                 self.addData(inpt)
